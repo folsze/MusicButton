3e4b506a402636ea2f8c24722072dbfab0581643
folsze <81622754+folsze@users.noreply.github.com>
2023-01-03 16:54:08 +0100

    Revert "working but still bug with bad quality"
    This reverts commit d0bb73a3af8f445d64a3effdd90d727d3acd803f.

--- app/src/main/java/com/example/musicbutton/MainActivity.kt
+++ app/src/main/java/com/example/musicbutton/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.example.musicbutton
-
-import android.media.MediaPlayer
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -12,13 +12,29 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.musicbutton.ui.theme.MusicButtonTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
+
+/*
+You can use the following code for commercial purposes with some restrictions.
+Read the full license here: https://semicolonspace.com/semicolonspace-license/
+For more designs with source code,
+visit: https://semicolonspace.com/jetpack-compose-samples/
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,49 +52,150 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private const val TAG = "FULIX"
-private const val pausedIcon = 1
-private const val playingIcon = 2
+private const val playIcon = 1
+private const val loadingBar = 2
+private const val pauseIcon = 3
 
 @Composable
-fun MusicButton() {
-    var buttonIcon by remember { mutableStateOf(pausedIcon) }
+fun MusicButton(
+    viewModel: MyViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
+    context: Context = LocalContext.current.applicationContext
+) {
 
-    val mContext = LocalContext.current
-    val mediaPlayer = MediaPlayer.create(mContext, R.raw.audio)
+    // Initially song is not loaded
+    val songLoaded by viewModel.songLoaded.observeAsState(false)
 
-    FloatingActionButton(
-        modifier = Modifier.size(80.dp),
+    // This is used to remember the icon of the button
+    // Its values are playIcon, loadingBar, and pauseIcon
+    // Initially display the playIcon
+    var buttonIcon by remember {
+        mutableStateOf(playIcon)
+    }
+
+    OutlinedButton(
+        modifier = Modifier
+            .size(72.dp),
         shape = CircleShape,
-        backgroundColor = Color.Red,
+        contentPadding = PaddingValues(12.dp),
+        elevation = ButtonDefaults.elevation(4.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
+            backgroundColor = Color(0xFF35898F)
+        ),
+        border = BorderStroke(0.dp, Color.Transparent),
         onClick = {
-            if (buttonIcon == pausedIcon) {
-                Log.i(TAG, "start")
-                mediaPlayer.start()
-                buttonIcon = playingIcon
-            } else if (buttonIcon == playingIcon) {
-                Log.i(TAG, "stop")
-                mediaPlayer.pause()
-                buttonIcon = pausedIcon
+            if (!songLoaded) {
+                // If song is NOT loaded
+
+                Toast.makeText(context, "Loading...", Toast.LENGTH_SHORT).show()
+
+                viewModel.loadTheSong()
+
+                // Assign buttonIcon to loadingBar so that progress indicator is displayed
+                buttonIcon = loadingBar
+            } else {
+                // If the song is already loaded
+
+                if (buttonIcon == playIcon) {
+                    // If the current icon is play Icon
+                    // change it to pause icon
+                    buttonIcon = pauseIcon
+                } else if (buttonIcon == pauseIcon) {
+                    // If the current icon is pause icon
+                    // change it to play icon
+                    buttonIcon = playIcon
+                }
             }
         }
     ) {
+
         when (buttonIcon) {
-            pausedIcon -> {
-                Icon(
-                    Icons.Filled.PlayArrow,
-                    modifier = Modifier.size(40.dp),
-                    contentDescription = "Play Song",
-                    tint = Color.White
-                )
+            loadingBar -> {
+                if (songLoaded) {
+                    // If the song is already loaded
+                    // set the pause icon
+                    buttonIcon = pauseIcon
+                } else {
+                    // If the song is NOT loaded
+                    // show the progress indicator
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        color = Color.White
+                    )
+                }
             }
-            playingIcon -> {
-                Icon(
-                    Icons.Filled.Pause,
-                    modifier = Modifier.size(40.dp),
-                    contentDescription = "Pause Song",
-                    tint = Color.White)
+            playIcon -> {
+                // Set the play icon
+                SetButtonIcon1(
+                    icon = Icons.Filled.PlayArrow,
+                    iconDescription = "Play Song"
+                )
+
+                // If the song is loaded, pause the actual song
+                if (songLoaded) {
+                    pauseTheSong(context = context)
+                }
+            }
+            pauseIcon -> {
+                // Set the pause icon
+                SetButtonIcon1(Icons.Filled.Pause, iconDescription = "Pause Song")
+
+                // If the song is loaded, play the actual song
+                if (songLoaded) {
+                    playTheSong(context = context)
+                }
             }
         }
+    }
+}
+
+private fun playTheSong(context: Context) {
+    // Here play the song
+    Log.i("SemicolonSpace", "playTheSong()")
+    Toast.makeText(context, "Playing....", Toast.LENGTH_SHORT).show()
+}
+
+private fun pauseTheSong(context: Context) {
+    // Here pause the song
+    Log.i("SemicolonSpace", "pauseTheSong")
+    Toast.makeText(context, "Paused", Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+private fun SetButtonIcon1(
+    icon: ImageVector,
+    iconDescription: String
+) {
+    Icon(
+        modifier = Modifier
+            .fillMaxSize(),
+        imageVector = icon,
+        contentDescription = iconDescription,
+        tint = Color.White
+    )
+}
+
+class MyViewModel : ViewModel() {
+
+    // Music Button
+    val songLoaded = MutableLiveData<Boolean>()
+
+    fun loadTheSong() {
+
+        viewModelScope.launch {
+
+            withContext(Dispatchers.Default) {
+                // Perform background task  to get the song
+                // After the task is completed, call songLoadedSuccessful()
+                // Instead of background task, I'm setting 4 seconds delay
+                delay(4000)
+            }
+            songLoadedSuccessful()
+        }
+    }
+
+    private fun songLoadedSuccessful() {
+        songLoaded.value = true
     }
 }
